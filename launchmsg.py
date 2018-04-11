bc8905b7853a95b4296adeeec748241041dd9c8c
thekiller2000 <38141421+thekiller2000@users.noreply.github.com>
2018-04-11 18:38:16 +0100

    Add files via upload

---
+++ launchmsg.py
@@ -0,0 +1,8 @@
+print ("Your game will start soon")
+print ("some problem? Type error, if it is something else write other")
+r = input ("Write the problem, if any")
+if r == 'error':
+ print ("please restart your game")
+elif r == "other":
+ print ("Search on google idiot")
+l= input (".")
