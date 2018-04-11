8ef06b8384da5af326e4c21797a3e0deac3d9710
thekiller2000 <38141421+thekiller2000@users.noreply.github.com>
2018-04-11 18:23:23 +0100

    Create launchmsg.py

---
+++ launchmsg.py
@@ -0,0 +1,7 @@
+print ("Your game will start soon")
+print ("some problem? Type error, if it is something else write other")
+r=input ("Write the problem, if any")
+if r== ("error")
+ print ("please restart your game")
+elif r== ("other")
+ print ("Search on google idiot")
