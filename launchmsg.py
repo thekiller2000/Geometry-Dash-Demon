458f64329ad71f06bb2e212ebf3069cadbeccb8e
thekiller2000 <38141421+thekiller2000@users.noreply.github.com>
2018-04-11 18:37:37 +0100

    Delete launchmsg.py

--- launchmsg.py
+++
@@ -1,7 +0,0 @@
-print ("Your game will start soon")
-print ("some problem? Type error, if it is something else write other")
-r=input ("Write the problem, if any")
-if r== ("error")
- print ("please restart your game")
-elif r== ("other")
- print ("Search on google idiot")
